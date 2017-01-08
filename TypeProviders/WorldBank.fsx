ee35eb206259540d82ce9a250f88c50333405f55
Olexandr Nikolaevsky <kanary@inbox.ru>
2017-01-09 00:20:42 +0200

    Get green electricity in countries

--- TypeProviders/WorldBank.fsx
+++ TypeProviders/WorldBank.fsx
@@ -10,6 +10,27 @@ let countries = wb.Countries
 
 let pop2000 = [for c in countries -> c.Indicators.``Population, total``.[2000]]
 let pop2010 = [for c in countries -> c.Indicators.``Population, total``.[2010]]
-let pop2015 = [for c in countries -> c.Indicators.``Population, total``.[2015]]
+let pop2013 = [for c in countries -> (c.Name, c.Indicators.``Electricity production from renewable sources, excluding hydroelectric (% of total)``.[2013], c.Indicators.``Electricity production from renewable sources, excluding hydroelectric (kWh)``.[2013])]
+let pop2014 = [for c in countries -> (c.Name, c.Indicators.``Electricity production from renewable sources, excluding hydroelectric (% of total)``.[2014], c.Indicators.``Electricity production from renewable sources, excluding hydroelectric (kWh)``.[2014])]
+let pop2015 = [for c in countries -> (c.Name, c.Indicators.``Electricity production from renewable sources, excluding hydroelectric (% of total)``.[2015], c.Indicators.``Electricity production from renewable sources, excluding hydroelectric (kWh)``.[2015])]
 
-pop2015 |> Seq.filter (fun v -> v > 0.0) |> Seq.sum
+let filterExistingData (dataList:(string*float*float) list) = 
+    let sortedList = 
+        dataList 
+        |> List.filter (fun (_,pros,_) -> pros > 0.0)
+        |> List.sortBy (fun (_,pros,_) -> -pros)
+
+    let count = min (sortedList |> List.length) 10
+
+    let top10 = 
+        sortedList
+        |> List.take count
+
+    dataList 
+    |> List.filter (fun (name,_,_) -> name = "Ukraine")
+    |> List.append top10
+
+
+let greenEl2013 = filterExistingData pop2013
+let greenEl2014 = filterExistingData pop2014
+let greenEl2015 = filterExistingData pop2015
