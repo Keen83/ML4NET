0650f4399483b9585ce59ec11c9ab8bb7f2e7021
Olexandr Nikolaevsky <kanary@inbox.ru>
2017-01-15 20:39:48 +0200

    Add Deedle libs

--- TypeProviders/UseRProvider.fsx
+++ TypeProviders/UseRProvider.fsx
@@ -11,6 +11,7 @@ open FSharp.Data
 
 let wb = WorldBankData.GetDataContext ()
 let countries = wb.Countries
+let pop2000 = [for c in countries -> c.Indicators.``Population, total``.[2000]]
 let pop2010 = [for c in countries -> c.Indicators.``Population, total``.[2010]]
 let surface = [for c in countries -> c.Indicators.``Surface area (sq. km)``.[2010]]
 
@@ -20,3 +21,19 @@ R.hist surface
 R.hist (surface |> R.log)
 
 R.plot (surface, pop2010)
+
+let pollution = [for c in countries -> c.Indicators.``CO2 emissions (kt)``.[2000]]
+let education = [for c in countries -> c.Indicators.``School enrollment, secondary (gross), gender parity index (GPI)``.[2000]]
+
+let rdf = 
+    [ "Pop2000", box pop2000
+      "Pop2010", box pop2010
+      "Surface", box surface
+      "Pollution", box pollution
+      "Education", box education ]
+    |> namedParams
+    |> R.data_frame
+
+rdf |> R.plot
+
+rdf |> R.summary |> R.print
